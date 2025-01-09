0bba3a5a5d4300cc5c23f70888fc29ae1dcf29b2
meryem-uludagli <meryemuslu2035@gmail.com>
2025-01-09 15:15:15 +0200

    add

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./hero";
+import Statistics from "./statistics";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Statistics />
     </div>
   );
 };
