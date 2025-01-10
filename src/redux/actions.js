8a4eefaeef324a9335cce71e4b346c66e61544d3
meryem-uludagli <meryemuslu2035@gmail.com>
2025-01-10 14:57:49 +0200

    add

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,27 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getDetails = createAsyncThunk("/covid/getDetails", (country) => {
-  api.get("");
-  return "PAYLOAD";
-});
+export const getDetails = createAsyncThunk(
+  "/covid/getDetails",
+  async (country) => {
+    const res = await api.get("/statistics", {
+      params: {
+        country,
+      },
+    });
+
+    const req2 = axios.get(`https://restcountries.com/v3.1/name/${country}`);
+
+    let data = res.data.response[0];
+
+    data = {
+      continent: data.continent,
+      country: data.country,
+      day: data.day,
+      cases: data.cases.total,
+      deaths: data.deaths.total,
+      tests: data.tests.total,
+      population: data.population,
+    };
+    return data;
+  }
+);
--- src/redux/covidSlice.js
+++ src/redux/covidSlice.js
@@ -9,6 +9,19 @@ createSlice({
   name: "covid",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(getDetails.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getDetails.rejected, (state, { error }) => {
+      state.isLoading = false;
+      state.error = error.messsage;
+    });
+    builder.addCase(getDetails.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.error = null;
+      state.data = payload;
+    });
+  },
 });
 export default covidSlice.reducer;
